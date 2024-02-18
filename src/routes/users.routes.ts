620d90fc6cb6b2c514ffb8181594c40863748ea4
FernandoMoyano <fernandomoyano21@gmail.com>
2024-02-18 17:35:36 -0300

    UserService-createUser

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,6 +2,8 @@ import express, { Router } from 'express'
 import { BookService } from '../services/book.service'
 
 export const userRouter: Router = express.Router()
+
+//Instancia de BookService
 const bookService = new BookService()
 
 userRouter.get('/')
