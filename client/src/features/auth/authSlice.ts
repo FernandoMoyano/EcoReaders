ef420bca982d8cebbc50c3752da40faf2f0310f7
FernandoMoyano <fernandomoyano21@gmail.com>
2024-03-01 13:11:04 -0300

    api.ts | authSlice.ts | store.ts

---
+++ client/src/features/auth/authSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    token: null,
+    user: null,
+  },
+  reducers: {
+    setToken: (state, action) => {
+      state.token = action.payload
+    },
+
+    setUser: (state, action) => {
+      state.user = action.payload
+    },
+  },
+})
+
+export const { setToken, setUser } = authSlice.actions
+export default authSlice.reducer
