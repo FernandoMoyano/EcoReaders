9cfd3c396d38c50254eea425e005d8e10775d73d
FernandoMoyano <fernandomoyano21@gmail.com>
2024-07-29 15:50:04 -0300

    modificando tamaños de las cards de libro

--- client/src/pages/Books.tsx
+++ client/src/pages/Books.tsx
@@ -27,26 +27,28 @@ const Books: React.FC = () => {
   return (
     <div>
       <NavBar />
-      <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-20 mb-5">
+      <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-20 mb-5">
         {data?.books?.map((book: IBook) => (
           <div
             key={book.id}
-            className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+            className="w-60  bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
           >
             <Link to={`/books/${book.id}`}>
-              <img src={book.image} alt="Product" className="h-80 w-72 object-contain rounded-t-xl" />
-              <div className="px-4 py-3 w-72">
-                <span className="text-gray-400 mr-3 uppercase text-xs">{book.author}</span>
-                <p className="text-lg font-bold text-black truncate block capitalize">{book.title}</p>
-                <div className="flex items-center">
-                  <p className="text-lg font-semibold text-black cursor-auto my-3">${formatearNumero(book.price)}</p>
-                  <div className="ml-auto">
-                    <p className="flex flex-wrap end justify-end mr-1">
-                      publicado por:
-                      <span className="text-black font-bold ml-2">
-                        {book.publisherName || publishedBooks[book.id].postedByUser}
-                      </span>
-                    </p>
+              <div className="flex flex-col items-center">
+                <img src={book.image} alt="Product" className="h-60 w-54 object-contain rounded-t-xl" />
+                <div className="px-4 py-3 w-54 truncate">
+                  <span className="text-gray-400 mr-3 uppercase text-xs">{book.author}</span>
+                  <p className="text-base font-bold text-black truncate block capitalize">{book.title}</p>
+                  <div className="flex items-center">
+                    <p className="text-lg font-semibold text-black cursor-auto my-3">${formatearNumero(book.price)}</p>
+                    <div className="ml-auto">
+                      <p className="flex text-sm flex-wrap end justify-end mr-1">
+                        publicado por:
+                        <span className="text-black text-sm font-bold ml-2">
+                          {book.publisherName || publishedBooks[book.id].postedByUser}
+                        </span>
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
