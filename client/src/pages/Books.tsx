90256a4f88c98d07e55a7f612b19209bafef260f
FernandoMoyano <fernandomoyano21@gmail.com>
2024-07-29 21:57:12 -0300

    primera version del slider con React Slick

--- client/src/pages/Books.tsx
+++ client/src/pages/Books.tsx
@@ -9,6 +9,9 @@ import { Link } from 'react-router-dom'
 import { formatearNumero } from '../utilities'
 import { useSelector } from 'react-redux'
 import { RootState } from '../app/store'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 const Books: React.FC = () => {
   const { data, isLoading, isError } = useGetBooksQuery()
@@ -24,37 +27,77 @@ const Books: React.FC = () => {
     return <div>Ocurrió un error inesperado</div>
   }
 
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 600,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  }
+
   return (
     <div>
       <NavBar />
-      <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-20 mb-5">
-        {data?.books?.map((book: IBook) => (
-          <div
-            key={book.id}
-            className="w-60  bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
-          >
-            <Link to={`/books/${book.id}`}>
-              <div className="flex flex-col items-center">
-                <img src={book.image} alt="Product" className="h-60 w-54 object-contain rounded-t-xl" />
-                <div className="px-4 py-3 w-54 truncate">
-                  <span className="text-gray-400 mr-3 uppercase text-xs">{book.author}</span>
-                  <p className="text-base font-bold text-black truncate block capitalize">{book.title}</p>
-                  <div className="flex items-center">
-                    <p className="text-lg font-semibold text-black cursor-auto my-3">${formatearNumero(book.price)}</p>
-                    <div className="ml-auto">
-                      <p className="flex text-sm flex-wrap end justify-end mr-1">
-                        publicado por:
-                        <span className="text-black text-sm font-bold ml-2">
-                          {book.publisherName || publishedBooks[book.id].postedByUser}
-                        </span>
-                      </p>
+      <section className="container m-auto mt-32 mb-5">
+        <div className="w-4/5 mx-auto">
+          <Slider {...settings}>
+            {data?.books?.map((book: IBook) => (
+              <div key={book.id} className="p-1">
+                <Link to={`/books/${book.id}`} className="block w-52">
+                  <div className="w-52 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl flex flex-col items-center">
+                    <img src={book.image} alt="Product" className="h-52 w-52 object-contain rounded-t-xl" />
+                    <div className="px-4 py-3 w-52 truncate">
+                      <span className="text-gray-400 mr-3 uppercase text-xs">{book.author}</span>
+                      <p className="text-base font-bold text-black truncate block capitalize">{book.title}</p>
+                      <div className="flex items-center">
+                        <p className="text-lg font-semibold text-black cursor-auto my-3">
+                          ${formatearNumero(book.price)}
+                        </p>
+                        <div className="ml-auto">
+                          <p className="flex text-sm flex-wrap end justify-end mr-1">
+                            publicado por:
+                            <span className="text-black text-sm font-bold ml-2">
+                              {book.publisherName || publishedBooks[book.id].postedByUser}
+                            </span>
+                          </p>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
+                </Link>
               </div>
-            </Link>
-          </div>
-        ))}
+            ))}
+          </Slider>
+        </div>
       </section>
       <ModalNewBook />
     </div>
